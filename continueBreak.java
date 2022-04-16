d200c53f79dcd574986e8ca9cf4e6444a62e08d8
hayet-dev <64528762+hayet-dev@users.noreply.github.com>
2022-04-16 23:14:54 +0200

    Add files via upload
    l'instruction Break

---
+++ continueBreak.java
@@ -0,0 +1,38 @@
+
+public class continueBreak {
+
+	public static void main(String[] args) {
+//	for (int i = 0; i < 5; i++) {
+//		if(i==5) {
+//		continue;
+//		
+//			 }
+//		System.out.println("le compte est de :"+i);
+//} 
+//	
+//	System.out.println("A la prochaine fois");
+//	
+//		
+//	System.out.println("a la prochaine");
+		int i=0;
+		while(i<10){
+		if(i==5){
+			i++;
+			continue;
+		}
+		System.out.println(i);
+		i++;
+		}
+		System.out.println("a la prochaine fois");
+	
+
+	
+	
+	
+	
+	
+	}
+	}
+			
+		
+
